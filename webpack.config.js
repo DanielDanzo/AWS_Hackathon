d69a699a6aed83661268f27eeadef5af862830f2
DanielDanzo <sempapadaniel123@gmaiil.com>
2024-07-27 18:03:54 +0200

    again x3

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: 'src/scripts/index.js',  // Your entry file
+  entry: './src/scripts/index.js',  // Your entry file
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')  // Ensure this is set to 'dist'
