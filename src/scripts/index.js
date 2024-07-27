8ae501bc10d7c8c09e08755564a431c2e6b78e89
DanielDanzo <sempapadaniel123@gmaiil.com>
2024-07-27 23:48:53 +0200

    wordsapi

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,2 +1,13 @@
 console.log('Hello')
 console.log('Mello')
+
+
+
+
+// These code snippets use an open-source library. http://unirest.io/nodejs
+unirest.get("https://wordsapiv1.p.mashape.com/words/soliloquy")
+.header("X-Mashape-Key", "<required>")
+.header("Accept", "application/json")
+.end(function (result) {
+  console.log(result.status, result.headers, result.body);
+});
