789d74f59876a7ef6112810fe441f1f6c3c4ca1a
DanielDanzo <sempapadaniel123@gmaiil.com>
2024-07-27 17:24:12 +0200

    welcome page done

--- src/modules/login.js
+++ src/modules/login.js
@@ -1,9 +1,9 @@
 import { signInWithPopup , GoogleAuthProvider, signOut} from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js"
-import { db, auth, provider } from './init.js';
+import { auth, provider } from './init.js';
 
 
 //FUNCTION: Registers user using their google email
-async function signInUser(pTag){
+async function signInUser(email, pTag){
     //sign-in using small window prompt
     signInWithPopup(auth, provider)
     .then(async (result) => {
@@ -13,9 +13,14 @@ async function signInUser(pTag){
         // The signed-in user info.
         //return result.user;
         const user = result.user;
-        const email = user.email;
+        const googleEmail = user.email;
 
-        window.location.href ='https://ambitious-glacier-0cd46151e.5.azurestaticapps.net/AdminUpdate.html';
+        if(email == googleEmail){
+            window.location.href ='https://localhost:5500/dashboard.html';
+        }else{
+            pTag.className = 'show';
+        }
+
  
         }).catch((error) => {
             // Handle Errors here.
@@ -25,3 +30,6 @@ async function signInUser(pTag){
         });
     
 }
+
+
+export { signInUser }
