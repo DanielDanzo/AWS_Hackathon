7a1bc0ceec3d4b55b7bded08e0e94dcf005d2e22
DanielDanzo <sempapadaniel123@gmaiil.com>
2024-07-27 18:01:13 +0200

    AWS build

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+  entry: 'src/scripts/index.js',  // Your entry file
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')  // Ensure this is set to 'dist'
+  },
+  // Other configurations...
+};
