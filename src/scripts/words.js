f594850b5beeb4d902b3d43883db69e396a87ddb
DanielDanzo <sempapadaniel123@gmail.com>
2024-07-28 13:02:46 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/scripts/words.js
+++ src/scripts/words.js
@@ -1,20 +1,21 @@
 
 
 
-
-const url = 'https://dictionary-by-api-ninjas.p.rapidapi.com/v1/dictionary?word=bright';
-const options = {
-	method: 'GET',
-	headers: {
-		'x-rapidapi-key': 'e3bcc24a89msh6a76c3446082eecp147343jsn384bce2dba2f',
-		'x-rapidapi-host': 'dictionary-by-api-ninjas.p.rapidapi.com'
+async function getDefinition(word){
+	const url = `https://dictionary-by-api-ninjas.p.rapidapi.com/v1/dictionary?word=${word}`;
+	const options = {
+		method: 'GET',
+		headers: {
+			'x-rapidapi-key': 'e3bcc24a89msh6a76c3446082eecp147343jsn384bce2dba2f',
+			'x-rapidapi-host': 'dictionary-by-api-ninjas.p.rapidapi.com'
+		}
+	};
+	
+	try {
+		const response = await fetch(url, options);
+		const result = await response.text();
+		console.log(result);
+	} catch (error) {
+		console.error(error);
 	}
-};
-
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
 }
