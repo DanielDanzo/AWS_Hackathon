963a8f404fd3fe08ffb20b65d13d5165780d74f1
DanielDanzo <sempapadaniel123@gmaiil.com>
2024-07-28 08:09:57 +0200

    combination

---
+++ index.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 8000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const uri = "mongodb+srv://TD-KMS:Starco05@cluster0.f46xymx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const client = new MongoClient(uri);
+
+app.post('/submit-student', async (req, res) => {
+    const studentData = req.body;
+
+    try {
+        await client.connect();
+        const result = await createStudent(client, studentData);
+        res.status(201).send(`New student created with the following id: ${result.insertedId}`);
+    } catch (error) {
+        res.status(500).send(error.message);
+    } finally {
+        await client.close();
+    }
+});
+
+app.get('/courses', async (req, res) => {
+    try {
+        await client.connect();
+        const courses = await getCourses(client);
+        res.status(200).json(courses);
+    } catch (error) {
+        res.status(500).send(error.message);
+    } finally {
+        await client.close();
+    }
+});
+
+app.get('/events', async (req, res) => {
+    try {
+        await client.connect();
+        const events = await getEvents(client);
+        res.status(200).json(events);
+    } catch (error) {
+        res.status(500).send(error.message);
+    } finally {
+        await client.close();
+    }
+});
+
+app.get('/bursaries', async (req, res) => {
+    try {
+        await client.connect();
+        const bursaries = await getBursaries(client);
+        res.status(200).json(bursaries);
+    } catch (error) {
+        res.status(500).send(error.message);
+    } finally {
+        await client.close();
+    }
+});
+
+async function createStudent(client, newStudent) {
+    const result = await client.db("test").collection("students").insertOne(newStudent);
+    return result;
+}
+
+async function getCourses(client) {
+    const cursor = await client.db("test").collection("courses").find({});
+    const courses = await cursor.toArray();
+    return courses;
+}
+
+async function getEvents(client) {
+    const cursor = await client.db("test").collection("events").find({});
+    const events = await cursor.toArray();
+    return events;
+}
+
+async function getBursaries(client) {
+    const cursor = await client.db("test").collection("bursaries").find({});
+    const bursaries = await cursor.toArray();
+    return bursaries;
+}
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
---
+++ src/scripts/words.js
@@ -0,0 +1,22 @@
+console.log('Hello')
+console.log('Mello')
+
+
+
+/*
+const url = 'https://dictionary-by-api-ninjas.p.rapidapi.com/v1/dictionary?word=bright';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'e3bcc24a89msh6a76c3446082eecp147343jsn384bce2dba2f',
+		'x-rapidapi-host': 'dictionary-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}*/
